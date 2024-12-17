33d8d9790f27a34591f37755f122f4360e9dbde7
RayJi <kaikai0428@gmail.com>
2024-12-17 08:08:45 +0800

    update

--- 切圖.py
+++ 切圖.py
@@ -3,11 +3,12 @@ import numpy as np
 import json
 import sys
 import os
+import random
 
 if len(sys.argv) == 1:
     # test
-    json_file = f'{os.getcwd()}\\jp1_font.fnt'
-    png_file = f'{os.getcwd()}\\jp1_font.png'
+    json_file = f'{os.getcwd()}\\029b99303.json'
+    png_file = f'{os.getcwd()}\\129b99303.png'
 else:
     if '.png' in sys.argv[1]:
         png_file = sys.argv[1]
@@ -48,8 +49,8 @@ if 'frames' in json_data:
         copy = img[y:y+h, x:x+w]
 
         # 將目標版面以中心擴展為原始圖尺寸
-        dst = cv2.copyMakeBorder(copy, oy, sh-h-oy, ox,
-                                 sw-w-ox, cv2.BORDER_CONSTANT)
+        dst = cv2.copyMakeBorder(
+            copy, oy, sh-h-oy, ox, sw-w-ox, cv2.BORDER_CONSTANT)
 
         # 另存圖片
         # cv2.imwrite(output_dir + name + '.png', dst)
@@ -102,5 +103,37 @@ elif 'mc' in json_data:
         # 中文檔名儲存會有問題，改用imencode代替
         # https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/356918/
         cv2.imencode('.png', copy)[1].tofile(output_dir + name + '.png')
+elif json_data[3] and json_data[3][0] == 'cc.SpriteFrame':
+    idx = 0
+    for frame_data in json_data[5]:
+        frame = frame_data[0][0]
+        name = frame['name']  # 名稱
+        if name == '':
+            name = str(idx)
+            idx += 1
+        x = frame['rect']['x']  # 位於spritesheet x座標
+        y = frame['rect']['y']  # 位於spritesheet y座標
+        w = frame['rect']['width']  # 位於spritesheet 寬
+        h = frame['rect']['height']  # 位於spritesheet 高
+        ox = frame['offset']['x']  # 位於spritesheet 裁減區和原圖x偏移量
+        oy = frame['offset']['y']  # 位於spritesheet 裁減區和原圖y偏移量
+        sw = frame['originalSize']['width']  # 原圖寬度(含透明區)
+        sh = frame['originalSize']['height']  # 原圖高度(含透明區)
+
+        if frame['rotated'] == True:
+            w, h = h, w
+            sw, sh = sh, sw
+        # 從spritesheet切出目標
+        copy = img[y:y+h, x:x+w]
+
+        # 將目標版面以中心擴展為原始圖尺寸
+        dst = cv2.copyMakeBorder(
+            copy, oy, sh-h-oy, ox, sw-w-ox, cv2.BORDER_CONSTANT)
+        dst = cv2.rotate(dst, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        # 另存圖片
+        # cv2.imwrite(output_dir + name + '.png', dst)
+        # 中文檔名儲存會有問題，改用imencode代替
+        # https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/356918/
+        cv2.imencode('.png', dst)[1].tofile(output_dir + name + '.png')
 
 input("done!")
